f9b173a8856d80ee0b801952ee7ebce83c0cd014
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-11 13:42:26 -0500

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -15,7 +15,8 @@ test("When the user fills out and submits form, species appears in animal list",
     //Act:
     // - get the species input and add in a value
     const speciesInput = screen.getByLabelText("Species:");
-
+    userEvent.type(speciesInput, "kitty");
+    
     // - get the age input and add in a value
     const ageInput = screen.getByLabelText("Age:");
 
