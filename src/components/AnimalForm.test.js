7a38ed3831cdbbcef28e18a223fa8d7702b9a949
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-11 13:59:52 -0500

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -33,10 +33,10 @@ test("When the user fills out and submits form, species appears in animal list",
 
     //Asset:
     // our species name should be on the the screen.
-    // const animalText = await screen.findByText(/kitty/i);
-    // expect(animalText).toBeInTheDocument();
+    const animalText = await screen.findByText(/kitty/i);
+    expect(animalText).toBeInTheDocument();
 
-    await waitFor(()=>{
+    await waitFor(() => {
         const animalText = screen.queryByText(/kitty/i);
         expect(animalText).toBeInTheDocument();
     });
