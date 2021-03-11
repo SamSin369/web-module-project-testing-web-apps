24d1de32cbcf6720b30cb85ecea85c85379bfbaf
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-11 12:52:06 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,17 @@
 
 
+it("Name of the test", ()=> {
+    console.log("Does the thing.");
+});
+
 test("Name of the test", ()=> {
     console.log("Does the thing.");
-})
+});
+
+test("Name of the test", ()=> {
+    console.log("Does the thing.");
+});
+
+test("Name of the test", ()=> {
+    console.log("Does the thing.");
+});
