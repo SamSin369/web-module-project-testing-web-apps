1dfe20446aa61b75b21de93071810f74568c9da3
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-11 13:58:18 -0500

    lecture work-in-progress

--- src/components/AnimalForm.test.js
+++ src/components/AnimalForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen, render } from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 
 import AnimalForm from './AnimalForm';
