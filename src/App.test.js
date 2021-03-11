f9cd841a6c991380984d73dac560fbd49ec1fe23
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-03-11 12:53:40 -0500

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,21 @@
 
 
-test("Name of the test", ()=> {
-    console.log("Does the thing.");
-});
+// describe("Validation tests", ()=> {
+//     test("Name of the test", ()=> {
+//         console.log("Does the thing.");
+//     });
+    
+//     test("Name of the test", ()=> {
+//         console.log("Does the thing.");
+//     });    
+// });
 
-test("Name of the test", ()=> {
-    console.log("Does the thing.");
-});
-
-test("Name of the test", ()=> {
-    console.log("Does the thing.");
-});
-
-test("Name of the test", ()=> {
-    console.log("Does the thing.");
-});
+// describe("Edge Cases", ()=> {
+//     test("Name of the test", ()=> {
+//         console.log("Does the thing.");
+//     });
+    
+//     test("Name of the test", ()=> {
+//         console.log("Does the thing.");
+//     });    
+// });
